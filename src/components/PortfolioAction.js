3659710ce977ea07ac6708744941dbc795620c9e
Taosif Hossain <112568801+mdtaosifhossain3@users.noreply.github.com>
2024-08-16 23:54:50 +0600

    Update PortfolioAction.js

--- src/components/PortfolioAction.js
+++ src/components/PortfolioAction.js
@@ -29,7 +29,7 @@ const PortfolioAction = () => {
     <div className="portfolio-action">
       <Button
         target="__blank"
-        link="https://github.com/zonayedpca"
+        link="https://github.com/mdtaosifhossain3"
         onMouseOver={handleMouseOver}
         onMouseOut={handleOnMouseOut}
         onClick={handleClick}

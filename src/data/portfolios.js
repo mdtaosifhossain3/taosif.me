cfa5ae98b56f05f54de1461989c31e8817fe34f1
Taosif Hossain <112568801+mdtaosifhossain3@users.noreply.github.com>
2024-08-17 11:49:31 +0600

    Update portfolios.js

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -5,7 +5,7 @@ export const portfolios = {
       name: "ChatBot Ai",
       description:
         "This project is a mobile application similar to GeminiAI, developed using Flutter. It includes features such as Firebase for database management, Provider for state management, animations, auto-scroll, authentication, and support for light and dark modes.",
-  source: "https://github.com/devsonket/devsonket.github.io",
+  source: "https://github.com/mdtaosifhossain3/flutter-chatbot-ai",
       tools: ["firebase", "provider"],
     },
     {
@@ -22,120 +22,65 @@ export const portfolios = {
       name: "E-Commerce App",
   
       description:
-        "an Ecommerce App",
-      demo: "https://zonayed.js.org",
-      source: "https://github.com/zonayedpca/js.zonayed.me",
-      tools: ["gatsby", "utterances", "travis ci"],
+        "A Mini Ecommerce App",
+  
+      source: "https://github.com/mdtaosifhossain3/flutter-mini-ecommerce",
+      tools: ["Provider", "firebase", "carousel slider"],
+    },
+    {
+      type: "featured",
+      name: "Music Player",
+      description:
+        "A Music Player App",
+ 
+      source: "https://github.com/mdtaosifhossain3/flutter-music-player",
+      tools: ["firebase", "Getx"],
     },
     {
       type: "regular",
-      name: "Phonebook",
+      name: "E-Commerce App",
       description:
-        "A CRUD Application to store your contact phone numbers, everything at one place",
-      demo: "https://phonebookpca.netlify.com/",
-      source: "https://github.com/zonayedpca/nodeMySQLPhonebook",
-      tools: ["express", "mysql", "react"],
-    },
-    {
-      type: "regular",
-      name: "Holy Quran",
-      description:
-        "Read the Holy Quran with English Transaltion. Simple, basic and clean",
-      demo: "https://zonayedpca.github.io/HolyQuranReact/",
-      source: "https://github.com/zonayedpca/HolyQuranReact",
-      tools: ["react", "redux"],
+        "A simple and minimal e-commerce App",
+      source: "https://github.com/mdtaosifhossain3/flutter-ecommerce",
+      tools: ["firebase", "Getx"],
     },
     {
       type: "regular",
       name: "To-do App",
       description:
         "Just another interactive, nice, beautiful and small to-do application. Does everything!",
-      demo: "https://todoreacthooks.netlify.com/",
-      source: "https://github.com/zonayedpca/todo-react-hooks",
-      tools: ["react", "react hooks"],
+     
+      source: "https://github.com/mdtaosifhossain3/flutter-todo-app",
+      tools: ["flutter", "dart"],
     },
     {
       type: "regular",
-      name: "Micro Blog",
+      name: "Covid Tracker App",
       description:
-        "Entirely works on the client-side, behaves like actual micro blog, everything handled by client",
-      demo: "https://zonayedpca.github.io/react-micro-blog/",
-      source: "https://github.com/zonayedpca/react-micro-blog",
-      tools: ["react", "react router"],
+        "Flutter COVID-19 Tracker: Track global COVID-19 cases and information",
+      
+      source: "https://github.com/mdtaosifhossain3/flutter-covid-tracker-app",
+      tools: ["Apis", "animations"],
     },
-    {
-      type: "regular",
-      name: "Mini Blog",
-      description:
-        "Entirely works on the client-side, behaves like actual blog, can even make posts, bookmarks",
-      demo: "https://zonayedpca.github.io/react-mini-blog/",
-      source: "https://github.com/zonayedpca/react-mini-blog",
-      tools: ["react", "react router", "draft js"],
-    },
-    {
-      type: "regular",
-      name: "PWA Blog",
-      description:
-        "A Simple blog powered by WP-Rest API and React, Progressive Web App supported",
-      demo: "https://zonayedjsv1.netlify.com",
-      source: "https://github.com/zonayedpca/old.js.zonayed.me",
-      tools: ["react", "pwa"],
-    },
+
   ],
   React: [
     {
       type: "featured",
-      name: "DevTop",
-      image: require("./image/devtop.jpg"),
+      name: "Roll the dice",
       description:
-        "Cross-platform desktop tool for the developers, does some useful job and make life easy",
-      demo: "https://zonayedpca.github.io/DevTop",
-      source: "https://github.com/zonayedpca/DevTop",
-      tools: ["electron", "react"],
+        "Roll the dice app",
+      source: "https://github.com/mdtaosifhossain3/react-native-roll-the-dice-app",
+      tools: ["JS", "react native"],
     },
     {
       type: "featured",
-      name: "Biswasi",
+      name: "Bg Changer App",
       image: require("./image/biswasi.jpg"),
       description:
-        "Cross-platform Mobile app for the Muslim people to remind them about their prayer",
-      demo: "https://play.google.com/store/apps/details?id=com.biswasi",
-      tools: ["react native", "redux"],
-    },
-    {
-      type: "featured",
-      name: "AlgoDS.js",
-      image: require("./image/algods.jpg"),
-      description:
-        "A Collection of most used algorithms and data structures implemented using JavaScript",
-      source: "https://github.com/zonayedpca/AlgoDS.js",
-      tools: ["data structure", "algorithm"],
-    },
-    {
-      type: "regular",
-      name: "React Native Welcome",
-      description:
-        "A helper package for React Native to welcome user with beautiful and interactive UI",
-      demo: "https://www.npmjs.com/package/react-native-welcome",
-      source: "https://github.com/zonayedpca/react-native-welcome",
-      tools: ["npm package", "react native"],
-    },
-    {
-      type: "regular",
-      name: "React Web Workers",
-      description: "A Small helper package for React to work with web workers",
-      demo: "https://www.npmjs.com/package/react-web-workers",
-      source: "https://github.com/zonayedpca/react-web-workers",
-      tools: ["npm package", "react"],
-    },
-    {
-      type: "regular",
-      name: "React Poll",
-      description:
-        "Entirely works on client-side, behave like actual poll application",
-      demo: "https://zonayedpca.github.io/react-fake-poll/",
-      source: "https://github.com/zonayedpca/react-fake-poll",
-      tools: ["react", "react router"],
+        "A bg changer app",
+       source: "https://github.com/mdtaosifhossain3/react-native-bgchanger-app",
+      tools: ["react native", "JS"],
     },
   ],
 }

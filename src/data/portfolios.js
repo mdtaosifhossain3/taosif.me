fad673a9f4855bea3c064e549796fd70ea616c8e
Taosif Hossain <112568801+mdtaosifhossain3@users.noreply.github.com>
2024-10-01 22:04:27 +0600

    Update portfolios.js

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -20,32 +20,18 @@ export const portfolios = {
       source: "https://github.com/mdtaosifhossain3/flutter-paypal-clone",
       tools: ["firebase", "GetX"],
     },
-    {
-      type: "regular",
-      name: "E-Commerce App",
-
-      description: "A Mini Ecommerce App",
-
-      source: "https://github.com/mdtaosifhossain3/flutter-mini-ecommerce",
-      tools: ["Provider", "firebase", "carousel slider"],
+     {
+      type: "featured",
+      image:
+        "https://github.com/mdtaosifhossain3/flutter-mini-ecommerce/blob/972b1fd97b847dffb7ca12ecf097007336a7f5f9/ecommerce_mockup.png",
+      name: "Ecommerce App",
+         source: "https://github.com/mdtaosifhossain3/flutter-mini-ecommerce",
+      description:
+        "An advanced e-commerce app built using Flutter, Firebase, and GetX for state management. The app provides a seamless shopping experience.",
+      tools: ["firebase", "getx"],
     },
-    {
-      type: "regular",
-      name: "Music Player",
-      description: "A Music Player App",
-
-      source: "https://github.com/mdtaosifhossain3/flutter-music-player",
-      tools: ["firebase", "Getx"],
-    },
-    {
-      type: "regular",
-      name: "E-Commerce App",
-      description: "A simple and minimal e-commerce App",
-      source: "https://github.com/mdtaosifhossain3/flutter-ecommerce",
-      tools: ["firebase", "Getx"],
-    },
-    {
-      type: "regular",
+        {
+      type: "featured",
       name: "To-do App",
       image:
         "https://raw.githubusercontent.com/mdtaosifhossain3/flutter-todo-app/6bb0254b0d7e185b43b761ab6f68f728943ab32f/flutter-todo-app-mockup.png",
@@ -55,7 +41,7 @@ export const portfolios = {
       source: "https://github.com/mdtaosifhossain3/flutter-todo-app",
       tools: ["flutter", "dart"],
     },
-    {
+     {
       type: "featured",
       name: "Covid Tracker App",
       image:
@@ -65,6 +51,40 @@ export const portfolios = {
 
       source: "https://github.com/mdtaosifhossain3/flutter-covid-tracker-app",
       tools: ["Apis", "animations"],
+    },
+    {
+      type: "featured",
+      name: "Tic Tac Toe App",
+      image:
+        "https://github.com/mdtaosifhossain3/flutter-tic-tac-toe-app/blob/5372b8953abe1e0186dcb4e080ea9b710a6bda5a/tic-tak-toe.png",
+      description:
+        "This project is a simple yet interactive Tic-Tac-Toe game built with Flutter. Users can enjoy playing against an AI opponent.",
+
+      source: "https://github.com/mdtaosifhossain3/flutter-tic-tac-toe-app",
+      tools: ["flutter", "dart"],
+    },
+    
+    {
+      type: "regular",
+      name: "Music Player",
+      description: "A Music Simple Player App. It allows users to play local and cloud songs, organize playlists, and search music seamlessly.",
+
+      source: "https://github.com/mdtaosifhossain3/flutter-music-player",
+      tools: ["firebase", "Getx"],
+    },
+    {
+      type: "regular",
+      name: "Freelancing Guide App",
+      description: "This is a mobile application built using Flutter that provides essential guides and tips for freelancers. ",
+      source: "https://github.com/mdtaosifhossain3/flutter-freelancing-guide-app",
+      tools: ["flutter", "provider"],
+    },
+    {
+      type: "regular",
+      name: "Note App",
+      description: "The Flutter Note App with SQLite allows users to create, edit, delete, and save notes locally on their device. It uses SQLite for persistent offline data storage, enabling note retrieval even when the app is closed.",
+      source: "https://github.com/mdtaosifhossain3/flutter-note-app-with-sqLite",
+      tools: ["firebase", "sqLite"],
     },
   ],
   React: [

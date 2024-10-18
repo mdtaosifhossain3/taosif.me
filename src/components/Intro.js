cd6586849f95934f9e5da461d3f3d5186ad86751
mdtaosifhossain3 <mdtaosifhossain3@gmail.com>
2024-10-18 22:46:30 +0600

    adding my image to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,11 +4,14 @@ import { Container, Title, Button } from "./common"
 
 import "./intro.css"
 
+import portfolio from "../assets/image/portfolio.png"
+
 const Intro = () => {
   return (
     <div className="intro-area">
       <Title title="Follow Me" social height="320px" />
       <Container>
+<<<<<<< HEAD
         <div className="intro">
           <div className="intro-text">
             <p>Hello! I am</p>
@@ -26,7 +29,52 @@ const Intro = () => {
                 title="Get Resume"
               />
               <Button clickEvent link="#about" ml="15px" title="About Me" />
+=======
+        <div
+          className="container"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            flexWrap: "wrap", // Makes it responsive
+            padding: "20px",
+          }}
+        >
+          <div className="intro">
+            <div className="intro-text">
+              <p>Hello! I am</p>
+              <h1>Muhammad Taosif</h1>
+              <h3>App Developer</h3>
+              <ul>
+                <li>Flutter Developer</li>
+                <li>React Native Developer</li>
+                <li>Programmer</li>
+              </ul>
+              <div className="action">
+                <Button
+                  link="https://github.com/mdtaosifhossain3/taosif.me/blob/11574c33b4c0e55df9e70f7cb5268767c3879e26/cc.pdf"
+                  target="__blank"
+                  bgColor="#00cf5d"
+                  title="Get Resume"
+                />
+                <Button clickEvent link="#about" ml="15px" title="About Me" />
+              </div>
+>>>>>>> 2153589 (adding my image to intro)
             </div>
+          </div>
+          <div className="imageContainer">
+            <img
+              className="image"
+              src={portfolio}
+              alt="Portfolio"
+              style={{
+                maxWidth: "100%",
+                height: "auto", // Makes the image responsive
+                objectFit: "cover",
+                borderRadius: "10px",
+              }}
+            />
           </div>
         </div>
       </Container>

3d62c9438df1a8b99a5b29b420addb3a6052698b
mdtaosifhossain3 <mdtaosifhossain3@gmail.com>
2024-10-18 22:41:11 +0600

    adding my image to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,25 +11,6 @@ const Intro = () => {
     <div className="intro-area">
       <Title title="Follow Me" social height="320px" />
       <Container>
-<<<<<<< HEAD
-        <div className="intro">
-          <div className="intro-text">
-            <p>Hello! I am</p>
-            <h1>Muhammad Taosif</h1>
-            <h3>App Developer</h3>
-            <ul>
-              <li>Flutter Developer</li>
-              <li>Programmer</li>
-            </ul>
-            <div className="action">
-              <Button
-                link="https://github.com/mdtaosifhossain3/taosif.me/blob/11574c33b4c0e55df9e70f7cb5268767c3879e26/cc.pdf"
-                target="__blank"
-                bgColor="#00cf5d"
-                title="Get Resume"
-              />
-              <Button clickEvent link="#about" ml="15px" title="About Me" />
-=======
         <div
           className="container"
           style={{
@@ -60,7 +41,6 @@ const Intro = () => {
                 />
                 <Button clickEvent link="#about" ml="15px" title="About Me" />
               </div>
->>>>>>> 2153589 (adding my image to intro)
             </div>
           </div>
           <div className="imageContainer">
